5daf23d9aa1359749ab36e992a16c078e5662104
David Ruiz Rodriguez <daruro@floridauniversitaria.es>
2024-03-05 10:06:57 +0100

    actividad 2

---
+++ 2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+nom=("Pepe" "Antonio" "Ana" "Pepe" "Antonio" "Ana" "Antonio")
+assig=("SOR" "FOL" "SOR" "SEG" "WEB" "SEG" "SEG")
+nota=(4 5 8 9 8 4 7)
+
+
+
+suspensos=0
+aprobados=0
+
+
+
+
+
+
+echo "Hay un total de $suspensos suspendidos y $aprobados aprobados."
